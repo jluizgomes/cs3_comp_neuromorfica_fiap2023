cf005d4f3871b4516b82eb3ed08c7f8d672e7998
Jorge Luiz Gomes <jluizgomes@gmail.com>
2023-10-31 13:47:57 -0300

    otimizaçao do codigo para melhor performance

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
+import time
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cs3neuro:cs3neuro2023@localhost/previsao_tempo_amazonia'
-
 db = SQLAlchemy(app)
 
-# Defina o modelo para a tabela do banco de dados
 class PrevisaoTempo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Data = db.Column(db.String(255))
@@ -22,27 +21,39 @@ class PrevisaoTempo(db.Model):
     Vento_Direcao = db.Column(db.String(255))
     Vento_Intensidade = db.Column(db.String(255))
 
-# Rota para configurações (pode ser estendida)
 @app.route('/configurations', methods=['POST'])
 def create_configuration():
-    # Implemente a lógica para configurar a ingestão dinâmica aqui
-    # Pode usar um modelo de dados para armazenar as configurações
     return jsonify({"message": "Configuração criada com sucesso"})
 
-# Rota para ler dados do CSV e inserir no banco de dados
 @app.route('/read', methods=['POST'])
 def read_csv():
+    start_time = time.time()  # Inicia a contagem do tempo
+    
+    csv_filename = "data/data_weather.csv"
+    
     try:
-        # Ler o CSV e obter os dados do arquivo especificado
-        csv_filename = "data/data_weather.csv"
+        # Ler o CSV
+        read_start_time = time.time()
         df = pd.read_csv(csv_filename)
-
+        read_end_time = time.time()
+        
         # Inserir os dados no banco de dados
+        insert_start_time = time.time()
         df.to_sql('previsao_tempo', con=db.engine, if_exists='replace', index=False)
-
-        return jsonify({"message": "Dados do CSV lidos e inseridos com sucesso"})
+        insert_end_time = time.time()
+        
+        message = {
+            "message": "Dados do CSV lidos e inseridos com sucesso",
+            "read_time": read_end_time - read_start_time,
+            "insert_time": insert_end_time - insert_start_time
+        }
+        
+        return jsonify(message)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    finally:
+        end_time = time.time()  # Encerra a contagem do tempo
+        print(f"Tempo total de execução: {end_time - start_time} segundos.")
 
 if __name__ == '__main__':
     db.create_all()
